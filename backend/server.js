30fb49f96c66b87a218e3795e7299c1ff815190b
ZTMYDarken <mizuhiro2003@gmail.com>
2026-05-03 22:50:00 +0200

    ajout folder docs

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const path    = require('path');
 const app  = express();
 const PORT = process.env.PORT || 3000;
 
+
 // ── Middlewares ──────────────────────────────────────────────
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
